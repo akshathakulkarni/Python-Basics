c7074e06067c3bc6d852408449bc6b99588e1907
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-05-04 14:55:27 -0400

    Import built-in modules to perform specific actions by using methods of te imported module

---
+++ modules.py
@@ -0,0 +1,8 @@
+# import math module to use pre-built functions for performing mathematical calculations.
+
+import math
+
+print("The square root of 16 is ", math.sqrt(16))
+
+print('Pi is ', math.pi)
+

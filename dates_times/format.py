f99c70da01330b7b580b47993f2272818ac4d90f
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-05-10 14:57:59 -0400

    Format dates and time in different ways using the built in date time classes

---
+++ dates_times/format.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+
+def main():
+
+
+    # Times and dates can be formatted using a set of predefined string
+    # control codes
+    now = datetime.now()
+
+    #### Date Formatting ####
+
+    # %y/%Y - Year, %a/%A - weekday, %b/%B - month, %d - day of month
+    print(now.strftime("The current weekday is:  %d"))
+    print(now.strftime("%A, %d, %b, %Y"))
+
+    # %c - locale's date and time, %x - locale's date, %X - locale's time
+    print(now.strftime("Locale date and time: %c"))
+    print(now.strftime("Locale date and time: %x"))
+    print(now.strftime("Locale date and time: %X"))
+
+    #### Time Formatting ####
+
+    # %I/%H - 12/24 Hour, %M - minute, %S - second, %p - locale's AM/PM
+    print(now.strftime("Current time: %I:%M:%S %p"))
+    print(now.strftime("Current time: %H:%M:%S %p"))
+
+if __name__ == "__main__":
+    main()
